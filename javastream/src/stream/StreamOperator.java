f45301cabce28486822f0fd5607dd6118876d00d
kimtjrgus <sh95921@gmail.com>
2023-01-09 00:59:50 +0900

    stream 연습1

---
+++ javastream/src/stream/StreamOperator.java
@@ -0,0 +1,26 @@
+package chapter.Stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamOperator {
+    public static void main(String[] args) {
+        // List 생성
+        List<String> fruitList = new ArrayList<>();
+        fruitList.add("김석현");
+        fruitList.add("이나무");
+        fruitList.add("박코딩");
+        
+        // ArrayList 생성
+        String[] fruitArray = {"김석현", "이나무", "박코딩"};
+
+        //각각 스트림 생성
+        Stream<String> ListStream = fruitList.stream();
+        Stream<String> ArrayStream = Arrays.stream(fruitArray);
+
+        ListStream.forEach(System.out::print);  // forEach :요소를 순회함.
+        ArrayStream.forEach(System.out::print);
+    }
+}
